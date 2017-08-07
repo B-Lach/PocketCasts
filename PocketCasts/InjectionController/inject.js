111b51d76b046860ea78c648097787cd7662e3a9
Benny Lach <benny@discollect.me>
2017-08-07 17:35:35 +0200

    Send Dictionary instead of String

--- PocketCasts/InjectionController/inject.js
+++ PocketCasts/InjectionController/inject.js
@@ -8,6 +8,6 @@ var element = angular.element(document.getElementById('audio_player')).scope()
 
 element.$watch('mediaPlayer.loaded', (newVal, oldVal) => {
   window.webkit.messageHandlers.mediaPlayer.postMessage(
-    JSON.stringify({'old': oldVal, 'new': newVal })
+    {'old': oldVal, 'new': newVal }
   )
 })
