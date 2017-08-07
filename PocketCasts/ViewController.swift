6b17befe7175b56f3430a25322824b6aeb508998
Benny Lach <benny@discollect.me>
2017-08-07 16:03:19 +0200

    Initial Commit

---
+++ PocketCasts/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  PocketCasts
+//
+//  Created by Benny Lach on 07.08.17.
+//  Copyright © 2017 DevKiste. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
