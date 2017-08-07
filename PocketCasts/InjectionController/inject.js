394728a676c735e8a7ad264d08f4d1ac1ac47569
Benny Lach <benny@discollect.me>
2017-08-07 18:48:38 +0200

    Updated javascript code

--- PocketCasts/InjectionController/inject.js
+++ PocketCasts/InjectionController/inject.js
@@ -2,12 +2,25 @@
 // TODO: - Validate jQuery was loaded
 $('.player_close').show()
 
-// Get the element we want to watch
-// TODO: - Validate angular is used
+// Get the element we want to add a watcher
+// TODO: - Validate angular is available
 var element = angular.element(document.getElementById('audio_player')).scope()
-
+// set the watcher
 element.$watch('mediaPlayer.loaded', (newVal, oldVal) => {
   window.webkit.messageHandlers.mediaPlayer.postMessage(
     {'old': oldVal, 'new': newVal }
   )
 })
+
+// handler definition
+var handlePlayPauseMediaKey = () => {
+    element.playPause()
+}
+
+var handleBackwardMediaKey = () => {
+    element.jumpBack()
+}
+
+var hanldeForwardMediaKey = () => {
+    element.jumpForward()
+}
