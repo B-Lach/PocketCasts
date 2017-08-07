970eb24b0db0cd1c92cfbf77adfc4dc4c0b14ecf
Benny Lach <benny@discollect.me>
2017-08-07 17:28:45 +0200

    Updated js script

--- PocketCasts/InjectionController/inject.js
+++ PocketCasts/InjectionController/inject.js
@@ -1,1 +1,13 @@
-console.log('hello world')
+// Show the close button - default is hidden
+// TODO: - Validate jQuery was loaded
+$('.player_close').show()
+
+// Get the element we want to watch
+// TODO: - Validate angular is used
+var element = angular.element(document.getElementById('audio_player')).scope()
+
+element.$watch('mediaPlayer.loaded', (newVal, oldVal) => {
+  window.webkit.messageHandlers.mediaPlayer.postMessage(
+    JSON.stringify({'old': oldVal, 'new': newVal })
+  )
+})
