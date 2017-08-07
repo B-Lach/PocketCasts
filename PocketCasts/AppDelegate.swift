889ae6f35803ee21cc6f034c9606f192cceae85a
Benny Lach <benny@discollect.me>
2017-08-07 18:51:30 +0200

    Changed window title

--- PocketCasts/AppDelegate.swift
+++ PocketCasts/AppDelegate.swift
@@ -14,6 +14,7 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
+        NSApplication.shared.keyWindow?.title = "Pocket Casts for Mac"
         // Insert code here to initialize your application
     }
 
